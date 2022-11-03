53a11583c800e286f02d1dc9257c16863477eaa8
adamb00 <borsodi.dm@gmail.com>
2022-11-03 07:00:16 +0100

    Initial message

---
+++ script.js
@@ -0,0 +1,60 @@
+const yearEl = document.querySelector('.year');
+yearEl.textContent = new Date().getFullYear();
+
+const btnNav = document.querySelector('.btn-mobile-nav');
+const headerEl = document.querySelector('.header');
+btnNav.addEventListener('click', () => {
+   headerEl.classList.toggle('nav-open');
+});
+const allLinks = document.querySelectorAll('a:link');
+allLinks.forEach(link =>
+   link.addEventListener('click', e => {
+      e.preventDefault();
+      const href = link.getAttribute('href');
+
+      if (href === '#') {
+         window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+         });
+      }
+      if (href !== '#' && href.startsWith('#')) {
+         const sectionEl = document.querySelector(href);
+         sectionEl.scrollIntoView({ behavior: 'smooth' });
+      }
+      if (link.classList.contains('main-nav-link'))
+         headerEl.classList.toggle('nav-open');
+   })
+);
+const sectionHeroEl = document.querySelector('.section-hero');
+const observer = new IntersectionObserver(
+   entries => {
+      const ent = entries[0];
+      if (ent.isIntersecting === false) document.body.classList.add('sticky');
+      if (ent.isIntersecting) document.body.classList.remove('sticky');
+   },
+   {
+      root: null,
+      threshold: 0,
+      rootMargin: '-80px',
+   }
+);
+observer.observe(sectionHeroEl);
+
+function checkFlexGap() {
+   var flex = document.createElement('div');
+   flex.style.display = 'flex';
+   flex.style.flexDirection = 'column';
+   flex.style.rowGap = '1px';
+
+   flex.appendChild(document.createElement('div'));
+   flex.appendChild(document.createElement('div'));
+
+   document.body.appendChild(flex);
+   var isSupported = flex.scrollHeight === 1;
+   flex.parentNode.removeChild(flex);
+   console.log(isSupported);
+
+   if (!isSupported) document.body.classList.add('no-flexbox-gap');
+}
+checkFlexGap();
